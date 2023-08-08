2583be0ceefb404932e3484bdd9e0f679a4471ec
Chitra <praisecathy@yahoo.com>
2023-08-08 15:25:41 +0100

    My First Project commit

---
+++ gitProject/src/test/java/utilities/TestRunnerClass.java
@@ -0,0 +1,17 @@
+package utilities;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber.html"}
+        , features = "src/test/java/features"
+        , glue = {"browserControl", "stepDefinitions"}
+)
+
+
+
+public class TestRunnerClass {
+
+}
